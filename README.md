598f0c930c5655c1d8af6f3f19bc2e5d792014ea
TH3CHARLie <th3charlie@gmail.com>
2019-05-15 00:03:32 +0800

    add program config

--- README.md
+++ README.md
@@ -1,20 +1,29 @@
 # no-tiger
 Not Ordinary Tiger Language
 
+## Introduction
 
-## Keywords
-- break, case, char, const, continue, default, do, double, else, float, for, if, int, long, return, signed, sizeof, static, struct, switch, typedef, union, unsigned, void, while
+no-tiger is the course project for Compiler, Zhejiang University, Spring 2019.
 
-## Unsupported Keywords
-
-- auto: default storage modifier
-- short: rarely used
-- goto: bad feature
-- extern: no intention to support multiple files yet
-- register, restrict, inline, volatile: no intention to support yet
+## Build
+```bash
+mkdir build && cd build && cmake3 ..
+make
+```
 
 ## Reference
 
 1. [ANSI-C-grammar-lex](http://www.quut.com/c/ANSI-C-grammar-l-2011.html)
 
 2. [ANSI-C-grammar-yacc](http://www.quut.com/c/ANSI-C-grammar-y-2011.html)
+
+
+## Roadmap
+
+- [x] Use Rust-like type names , eg: `f32, f64, u8, u16, u32, u64, i8, i16, i32, i64`
+- [ ] Support basic control flow
+- [ ] Support function
+- [ ] Support builtin array
+- [ ] Support custom data type
+- [ ] Support builtin I/O functions
+- [ ] For simplicity, I think we should not support pointer for now
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,88 @@
-#include "y.tab.c"
+#include <cxxopts.hpp>
+#include <iostream>
+enum class ProgramMode {
+  EMIT_LLVM_IR,
+  EMIT_ASSEMBLY,
+  EMIT_OBJECT,
+  EMIT_BINARY,
+};
 
-using namespace ntcc;
+struct ProgramConfig {
+  ProgramConfig() : mode(ProgramMode::EMIT_BINARY) {}
+  std::string input_filename;
+  std::string output_filename;
+  ProgramMode mode;
+};
+
+ProgramConfig parse_program_options(int argc, char* argv[]) {
+  using namespace cxxopts;
+  try {
+    cxxopts::Options options(argv[0], "- ntc: No-Tiger Lang Compiler`");
+    options.positional_help("[optional args]").show_positional_help();
+    options.add_options()("i, input", "Input file",
+                          cxxopts::value<std::string>(),
+                          "FILE")("l", "Emit llvm IR")(
+        "s", "Emit assembly code")("c", "Emit object code")(
+        "o, output", "Output file",
+        cxxopts::value<std::string>()->default_value("a"),
+        "FILE")("h, help", "Show help");
+    auto parse_result = options.parse(argc, argv);
+    if (parse_result.count("h")) {
+      std::cout << options.help({"", "Group"}) << std::endl;
+      exit(0);
+    }
+    ProgramConfig config_result;
+    if (parse_result.count("i")) {
+      std::string input_filename = parse_result["i"].as<std::string>();
+      config_result.input_filename = input_filename;
+    } else {
+      std::cerr << argv[0] << ": fatal no input file" << std::endl;
+      exit(4);
+    }
+    if (parse_result.count("s")) {
+      config_result.mode = ProgramMode::EMIT_ASSEMBLY;
+    }
+    if (parse_result.count("c")) {
+      config_result.mode = ProgramMode::EMIT_OBJECT;
+    }
+    if (parse_result.count("l")) {
+      config_result.mode = ProgramMode::EMIT_LLVM_IR;
+    }
+    if (parse_result.count("o")) {
+      std::string output_filename = parse_result["i"].as<std::string>();
+      config_result.output_filename = output_filename;
+    } else {
+      std::string output_filename = parse_result["i"].as<std::string>();
+      switch (config_result.mode) {
+        case ProgramMode::EMIT_BINARY: {
+          config_result.output_filename = output_filename + ".out";
+        }
+
+        break;
+        case ProgramMode::EMIT_ASSEMBLY: {
+          config_result.output_filename = output_filename + ".s";
+        }
+        break;
+        case ProgramMode::EMIT_OBJECT: {
+          config_result.output_filename = output_filename + ".o";
+        }
+        break;
+        case ProgramMode::EMIT_LLVM_IR: {
+          config_result.output_filename = output_filename + ".ll";
+        }
+        default:
+          break;
+      }
+    }
+    return config_result;
+
+  } catch (const cxxopts::OptionException& e) {
+    std::cerr << "Error: Parsing options: " << e.what() << std::endl;
+    exit(2);
+  }
+}
 
 int main(int argc, char* argv[]) {
-
+  ProgramConfig config = parse_program_options(argc, argv);
   return 0;
 }
